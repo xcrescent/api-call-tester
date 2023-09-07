7744ae2cfec5641d712e1fd8bace0ef4c6a2d671
Utkarsh Singh <usjadon19@gmail.com>
2023-09-08 02:18:57 +0530

    node version

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const port = process.env.port ?? 4000;
+const cors = require("cors");
+
+app.use(cors());
 
 app.use(express.static(__dirname + "/public"));
 app.use(express.json());
